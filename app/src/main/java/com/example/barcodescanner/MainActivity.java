093434f5e238f969e1ba25e4260e8578455a0350
vineelmathew <vineelrudrapati9@gmail.com>
2019-10-02 22:53:34 +0530

    first commit

---
+++ app/src/main/java/com/example/barcodescanner/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.barcodescanner;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        new IntentIntegrator(this).setBeepEnabled(false).initiateScan();
+    }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (result != null) {
+            if (result.getContents() == null) {
+                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this, "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
+                }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+}
