a2c4cd50cb5f6794d31e8b8a2f11ab4654e96000
vineelmathew <vineelrudrapati9@gmail.com>
2019-10-06 19:08:17 +0530

    added text view

--- app/src/main/java/com/example/barcodescanner/MainActivity.java
+++ app/src/main/java/com/example/barcodescanner/MainActivity.java
@@ -7,17 +7,20 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
 public class MainActivity extends AppCompatActivity {
+     TextView code;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        code=findViewById(R.id.code);
         new IntentIntegrator(this).setBeepEnabled(false).initiateScan();
     }
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -26,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
             if (result.getContents() == null) {
                 Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
             } else {
+                code.setText(result.getContents());
                 Toast.makeText(this, "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
                 }
         } else {
